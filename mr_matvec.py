e96ab53e9e1ddd6996e7ef06b56a4d0245549c6c
Ubuntu <ubuntu@ip-172-31-37-17.ec2.internal>
2026-09-21 21:32:51 +0000

    part b wip

--- mr_matvec.py
+++ mr_matvec.py
@@ -5,21 +5,23 @@ import joblib
 from mrjob.job import MRJob
 
 class MatVecProduct(MRJob):
-  def mapper(self, _, line):
-    if os.path.isfile("b_small.pkl"):
-      b = joblib.load("b_small.pkl")
-    else:
-      b = joblib.load("b_large.pkl")
-    line_elts = line.split()
-    row_index = int(line_elts[0])
-    col_index = int(line_elts[1])
-    A_val = float(line_elts[2])
-    # Your code here (use yield() in place of return())
-    yield row_index, A_val * b[col_index]
 
-  def reducer(self, key, values):
-    # Your code here (use yield() in place of return())
-    yield key, sum(values)
+    def mapper_init(self):
+        if os.path.isfile("b_small.pkl"):
+            self.b = joblib.load("b_small.pkl")
+        else:
+            self.b = joblib.load("b_large.pkl")
+
+    def mapper(self, _, line):
+        line_elts = line.split()
+        row_index = int(line_elts[0])
+        col_index = int(line_elts[1])
+        A_val = float(line_elts[2])
+
+        yield row_index, A_val * self.b[col_index]
+
+    def reducer(self, key, values):
+        yield key, sum(values)
 
 if __name__ == '__main__':
   start_time = datetime.now()
